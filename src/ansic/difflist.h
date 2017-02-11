9fed01123620415989ceaf165af3df2bad7d497c
Jörg Habenicht <jh@mwerk.net>
2017-02-11 18:53:01 +0100

    added GPL2 license to source code

--- src/ansic/difflist.h
+++ src/ansic/difflist.h
@@ -5,6 +5,28 @@
  *      Author: jh
  */
 
+/*
+    Container module to hold diff content of one file
+
+    Copyright (C) 2017  Jörg Habenicht (jh@mwerk.net)
+
+    This file is part of lfdiff
+
+    lfdiff is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    lfdiff is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef SRC_ANSIC_DIFFLIST_H_
 #define SRC_ANSIC_DIFFLIST_H_
 
